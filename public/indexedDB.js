1ab502f6067f31c7a241ad7fb9a92aeb75630f94
Sarah Hinton <smcfarland2@gmail.com>
2021-07-16 21:50:36 +1000

    added final functions to IndexedDB and linked to html

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -21,10 +21,55 @@ request.onerror = function (e) {
     console.log(`${e.target.errorCode}`);
 };
 
-// Open and access a transaction in the db
-let transaction = db.transaction(['budgetStore'], 'readwrite');
+function checkDatabase() {
+    // Open and access a transaction in the db
+    let transaction = db.transaction(['budgetStore'], 'readwrite');
 
-const store = transaction.objectStore('budgetStore');
+    const store = transaction.objectStore('budgetStore');
 
-// Getting all records from objectStore
-const getAll = store.getAll();
+    // Getting all records from objectStore
+    const getAll = store.getAll();
+
+    // If request successful, fetch json data and add to online db
+    getAll.onsuccess = function () {
+        if getAll.result.length > 0 {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then ((res) => res.json())
+            .then((res) => {
+                if (res.length !== 0) {
+                    transaction = db.transaction(['budgetStore'], 'readwrite');
+
+                    const currentStore = transaction.objectStore('budgetStore');
+
+                    currentStore.clear();
+                    console.log('Storage cleared!');
+                }
+            });
+        }
+    };
+}
+
+request.onsuccess = function (e) {
+    console.log('Succeeded');
+    db = e.target.result;
+    if (navigator.onLine) {
+        console.log('Connected to DB!');
+        checkDatabase();
+    }
+};
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(['budgetStore'], 'readwrite');
+    const store = transaction.objectStore('budgetStore');
+
+    store.add(record);
+};
+
+window.addEventListener('online', checkDatabase);
